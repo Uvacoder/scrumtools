6bcff68a043b9b29b4bfb8fb956491db08021f82
Amodio <pscamodio@gmail.com>
2021-07-29 10:25:23 +0000

    First data binding and conditional class

--- src/components/HostBox.vue
+++ src/components/HostBox.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>Host Config</div>
+  <div class="row">
+    <form class="col s12">
+      <h2>Host Session</h2>
+      <div class="input-field row">
+        <input id="hostName" v-model="hostName" type="text" />
+        <label for="hostName">Host Name</label>
+      </div>
+      <div class="btn row" :class="!canHost && 'disabled'">
+        Host Poker Session
+      </div>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { computed, ComputedRef, ref, Ref } from "vue";
+
+interface Data {
+  hostName: Ref<string>;
+  canHost: ComputedRef<boolean>;
+}
+
+export default {
+  data(): Data {
+    const hostName = ref("");
+    return {
+      hostName,
+      canHost: computed(() => {
+        return hostName.value.length > 0;
+      }),
+    };
+  },
+};
 </script>
