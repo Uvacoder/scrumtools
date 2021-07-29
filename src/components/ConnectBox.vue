fbf5fe5ac74b93e83b0333b863a40bc3f4e674bf
Amodio <pscamodio@gmail.com>
2021-07-29 10:46:27 +0000

    Sketch connect box page

--- src/components/ConnectBox.vue
+++ src/components/ConnectBox.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>Connection Config</div>
+  <div class="row">
+    <form class="col s12">
+      <h2>Connect</h2>
+      <div class="input-field row">
+        <input id="userName" v-model="userName" type="text" />
+        <label for="userName">User Name</label>
+      </div>
+      <div class="input-field row">
+        <input id="sessionToken" v-model="sessionToken" type="text" />
+        <label for="sessionToken">Session Token</label>
+      </div>
+      <div class="btn row" :class="!canConnect && 'disabled'">
+        Connect To Session
+      </div>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-export default {};
+import { computed, ref, Ref } from "vue";
+
+interface Data {
+  userName: Ref<string>;
+  sessionToken: Ref<string>;
+  canConnect: Ref<boolean>;
+}
+
+export default {
+  data(): Data {
+    const userName = ref("");
+    const sessionToken = ref("");
+    return {
+      userName,
+      sessionToken,
+      canConnect: computed(
+        () => userName.value.length > 0 && sessionToken.value.length > 0
+      ),
+    };
+  },
+};
 </script>
--- src/components/HostBox.vue
+++ src/components/HostBox.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="row">
     <form class="col s12">
-      <h2>Host Session</h2>
+      <h2>Host</h2>
       <div class="input-field row">
         <input id="hostName" v-model="hostName" type="text" />
         <label for="hostName">Host Name</label>
       </div>
-      <div class="btn row" :class="!canHost && 'disabled'">
+      <div class="btn row" :class="hostName.length == 0 && 'disabled'">
         Host Poker Session
       </div>
     </form>
@@ -14,11 +14,10 @@
 </template>
 
 <script lang="ts">
-import { computed, ComputedRef, ref, Ref } from "vue";
+import { ref, Ref } from "vue";
 
 interface Data {
   hostName: Ref<string>;
-  canHost: ComputedRef<boolean>;
 }
 
 export default {
@@ -26,9 +25,6 @@ export default {
     const hostName = ref("");
     return {
       hostName,
-      canHost: computed(() => {
-        return hostName.value.length > 0;
-      }),
     };
   },
 };
